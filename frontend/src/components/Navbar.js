e5e398fc3f513f7ecf2af5dd917f2ae2a6ccd4e4
hapsahusain05 <hapsahusain4@gmail.com>
2024-07-30 19:34:57 +0800

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Jika Anda ingin menambahkan styling khusus
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          MyApp
+        </Link>
+        <ul className="navbar-menu">
+          <li className="navbar-item">
+            <Link to="/" className="navbar-links">Home</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/about" className="navbar-links">About</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/services" className="navbar-links">Services</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/contact" className="navbar-links">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
 
