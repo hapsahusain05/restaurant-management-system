62da5542be0355747c585d556233ea8fa795122e
hapsahusain05 <hapsahusain4@gmail.com>
2024-07-30 19:59:22 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Import routes
+const userRoutes = require('./routes');
+
+// Middleware
+app.use(express.json()); // Untuk parsing JSON dari body request
+
+// Gunakan routes
+app.use('/api', userRoutes);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
