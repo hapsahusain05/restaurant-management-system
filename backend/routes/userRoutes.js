3c92f8d402f1a86e4e681b083b6676ff44bc1575
hapsahusain05 <hapsahusain4@gmail.com>
2024-07-30 19:20:22 +0800

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const userRoutes = require('./userRoutes');
+
+app.use(express.json()); // Untuk parsing JSON dari body request
+app.use('/api', userRoutes);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
 
