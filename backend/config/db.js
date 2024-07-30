0d1954e111d2673fb83e765cbcd27e8aa9964eff
hapsahusain05 <hapsahusain4@gmail.com>
2024-07-30 18:43:46 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Import koneksi database dari db.js
+const db = require('./db');
+
+// Contoh route untuk mendapatkan data dari database
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
 
