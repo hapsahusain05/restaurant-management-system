be2a0596bf9f46c5f1176e83131195351d40af24
hapsahusain05 <hapsahusain4@gmail.com>
2024-07-30 19:08:15 +0800

    Update errorMiddleware.js

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,1 +1,16 @@
+// Middleware untuk menangani kesalahan
+const errorMiddleware = (err, req, res, next) => {
+  console.error(err.stack); // Log kesalahan ke konsol
+
+  const statusCode = err.statusCode || 500; // Jika tidak ada statusCode, gunakan 500 sebagai default
+  const message = err.message || 'Internal Server Error'; // Jika tidak ada pesan, gunakan 'Internal Server Error' sebagai default
+
+  res.status(statusCode).json({
+    status: 'error',
+    statusCode,
+    message
+  });
+};
+
+module.exports = errorMiddleware;
 
