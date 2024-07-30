391447eeaadff3cf15f31bb636cecaad32dae071
hapsahusain05 <hapsahusain4@gmail.com>
2024-07-30 19:51:10 +0800

    Update userReducer.js

--- frontend/src/store/actions/reducers/userReducer.js
+++ frontend/src/store/actions/reducers/userReducer.js
@@ -1,1 +1,109 @@
+// Action Types
+const GET_USERS_REQUEST = 'GET_USERS_REQUEST';
+const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'GET_USERS_FAILURE';
+const CREATE_USER_REQUEST = 'CREATE_USER_REQUEST';
+const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS';
+const CREATE_USER_FAILURE = 'CREATE_USER_FAILURE';
+const UPDATE_USER_REQUEST = 'UPDATE_USER_REQUEST';
+const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS';
+const UPDATE_USER_FAILURE = 'UPDATE_USER_FAILURE';
+const DELETE_USER_REQUEST = 'DELETE_USER_REQUEST';
+const DELETE_USER_SUCCESS = 'DELETE_USER_SUCCESS';
+const DELETE_USER_FAILURE = 'DELETE_USER_FAILURE';
+
+// Initial State
+const initialState = {
+  users: [],
+  loading: false,
+  error: null,
+};
+
+// Reducer
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // Get Users
+    case GET_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case GET_USERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+      };
+    case GET_USERS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    // Create User
+    case CREATE_USER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case CREATE_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        users: [...state.users, action.payload],
+      };
+    case CREATE_USER_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    // Update User
+    case UPDATE_USER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case UPDATE_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        users: state.users.map(user =>
+          user.id === action.payload.id ? action.payload : user
+        ),
+      };
+    case UPDATE_USER_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    // Delete User
+    case DELETE_USER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case DELETE_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        users: state.users.filter(user => user.id !== action.payload),
+      };
+    case DELETE_USER_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
 
